653af5188b0d091ecabb8b91866c5d044e6b7896
mario-moscicki <69436611+mario-moscicki@users.noreply.github.com>
2023-07-06 22:26:15 +0200

    Update 1-test.spec.js
    added description for 2nd test

--- Udemy-Cypress- E2E-Web-UI-Automation/1-test.spec.js
+++ Udemy-Cypress- E2E-Web-UI-Automation/1-test.spec.js
@@ -29,6 +29,17 @@ describe("Let's start", () => {
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Form Layouts").click();
+
+    //Test case(learn-2): trying to jump on "Sign in" button from Horizontal form section in Form Layouts
+    //-go to tested site
+    //-from left menu click on form link
+    //-from drop down submenu click on form layouts
+    //-I used "Email" field class to come closer in DOM tree
+    //-I added more dependencies like -parent of this field
+    //-from DOM tree i asked to find "button"
+    //-t should contain "Sign in"
+    //-button must be parent of "form" class
+    
     cy.get("#inputEmail3")
       .parents("form")
       .find("button")
