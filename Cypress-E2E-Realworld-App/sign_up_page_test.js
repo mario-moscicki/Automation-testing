b2a18a59a0f291fb87416a4f33520983bc663557
mario-moscicki <69436611+mario-moscicki@users.noreply.github.com>
2023-12-08 15:25:56 +0100

    Create sign_up_page_test.js

---
+++ Cypress-E2E-Realworld-App/sign_up_page_test.js
@@ -0,0 +1,52 @@
+describe("Sign up page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("checking fields on Sign up page", () => {
+    //click on the link Dont have account - Sign Up
+    cy.contains("Sign Up").click();
+    //First name
+    cy.get("#firstName").type("aaaaa");
+    cy.wait(2000);
+    //Last name
+    cy.get("#lastName").type("aaaaa");
+    cy.wait(2000);
+    //Username
+    cy.get("#username").type("aaaaaa123");
+    cy.wait(2000);
+    //Password
+    cy.get("#password").type("123456789");
+    cy.wait(2000);
+    //Confirm password
+    cy.get("#confirmPassword").type("123456789");
+    cy.wait(2000);
+  });
+
+  it("checking if info about 'password below than 4 characters' & 'Password does not match' is visible", () => {
+    //click on the link Dont have account - Sign Up
+    cy.contains("Sign Up").click();
+    //First name
+    cy.get("#firstName").type("aaaaa");
+    cy.wait(2000);
+
+    //Last name
+    cy.get("#lastName").type("aaaaa");
+    cy.wait(2000);
+
+    //Username
+    cy.get("#username").type("aaaaaa123");
+    cy.wait(2000);
+
+    //Password
+    cy.get("#password").type("123");
+    cy.wait(2000);
+    cy.get("#password-helper-text").contains("Password must contain at least 4 characters");
+    cy.wait(2000);
+
+    //Confirm Password
+    cy.get("#confirmPassword").type("12345");
+    cy.wait(2000);
+    cy.get("#confirmPassword-helper-text").contains("Password does not match");
+  });
+});
